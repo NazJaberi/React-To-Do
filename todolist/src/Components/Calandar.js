9da38694c7476e6e16293221f5536ca8ff4990b7
njaberi <nezarajaberi@gmail.com>
2025-03-04 00:36:12 +0300

    Calendar fixes

--- todolist/src/Components/Calandar.js
+++ todolist/src/Components/Calandar.js
@@ -1,9 +1,31 @@
 import react from 'react'
+import { CalendarDate, CaretUp } from 'react-bootstrap-icons'
+import { calanderItems } from '../constants'
 
 function Calandar(){
+
     return(
-<div className='Calandar'>
-Calandar
+<div className='calendar'>
+    <div className="header">
+        <div className="title">
+            <CalendarDate size="18"/>
+            <p> Calender</p>
+        </div>
+        <div className="btns">
+            <span>
+                <CaretUp size="20" />
+            </span>
+        </div>
+        <div className="items">
+            {
+                calanderItems.map( item =>
+                    <div className="item" key={item}>
+                        {item}
+                    </div>
+                )
+            }
+        </div>
+</div>
 </div>
     )
 }
---
+++ todolist/src/constants/index.js
@@ -0,0 +1,1 @@
+export const calanderItems = ['today', 'next 7 days', 'all days']
