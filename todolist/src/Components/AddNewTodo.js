19f2c8587899f292afb0fb29dc8d0b13d1cfe25d
njaberi <nezarajaberi@gmail.com>
2025-03-03 21:29:15 +0300

    added Modal

--- todolist/src/Components/AddNewTodo.js
+++ todolist/src/Components/AddNewTodo.js
@@ -1,10 +1,27 @@
-import react from 'react'
+import React, { useState } from 'react'
+import Modal from './Modal'
 
 function AddNewTodo(){
+
+    const [showModal, setShowModal] = useState(false)
+
     return(
-<div className='AddNewTodo'>
-AddNewTodo
-</div>
+        <div className='AddNewTodo'>
+            <div className="btn">
+            <button onClick={() => setShowModal(true)}>
+                + New Todo
+            </button>
+            </div>
+            <Modal showModal={showModal} setShowModal={setShowModal}>
+                <div>
+                    Hello World
+                    <button onClick={() => setShowModal(false)}>
+                        hide
+                    </button>
+
+                </div>
+            </Modal>
+        </div>
     )
 }
 
