69685e6dd45f9fe29662bd334fccad073231dbab
njaberi <nezarajaberi@gmail.com>
2025-03-03 20:06:40 +0300

    User and sidebar adjustments and added some images

--- todolist/src/Components/User.js
+++ todolist/src/Components/User.js
@@ -1,10 +1,18 @@
 import react from 'react'
+import profile from "../images/profile.png"
 
 function User(){
+
     return(
-<div className='User'>
-User
-</div>
+        <div className='User'>
+            <div className='profile'>
+                <img src={profile} alt= "profile" />
+            </div>
+            <div className='info'>
+                <p> Nezar Jaberi</p>
+                <a href="#">Logout!</a>
+            </div>
+        </div>
     )
 }
 
