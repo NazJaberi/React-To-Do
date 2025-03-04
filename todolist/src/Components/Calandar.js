21d264486f9c92e6862e9cc116bd77e79ef27b9a
njaberi <nezarajaberi@gmail.com>
2025-03-04 04:06:53 +0300

    slight adjustment on calander

--- todolist/src/Components/Calandar.js
+++ todolist/src/Components/Calandar.js
@@ -3,30 +3,29 @@ import { CalendarDate, CaretUp } from 'react-bootstrap-icons'
 import { calanderItems } from '../constants'
 
 function Calandar(){
-
     return(
-<div className='calendar'>
-    <div className="header">
-        <div className="title">
-            <CalendarDate size="18"/>
-            <p> Calender</p>
+        <div className='calendar'>
+            <div className="header">
+                <div className="title">
+                    <CalendarDate size="18"/>
+                    <p>Calendar</p>
+                </div>
+                <div className="btns">
+                    <span>
+                        <CaretUp size="20" />
+                    </span>
+                </div>
+            </div>
+            <div className="items">
+                {
+                    calanderItems.map(item =>
+                        <div className="item" key={item}>
+                            {item}
+                        </div>
+                    )
+                }
+            </div>
         </div>
-        <div className="btns">
-            <span>
-                <CaretUp size="20" />
-            </span>
-        </div>
-        <div className="items">
-            {
-                calanderItems.map( item =>
-                    <div className="item" key={item}>
-                        {item}
-                    </div>
-                )
-            }
-        </div>
-</div>
-</div>
     )
 }
 
