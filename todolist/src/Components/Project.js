bbeacaeca95dad9bd994fccaabbd169425c4b64f
njaberi <nezarajaberi@gmail.com>
2025-03-04 05:17:10 +0300

    Porjects and project

--- todolist/src/Components/Project.js
+++ todolist/src/Components/Project.js
@@ -1,11 +1,44 @@
 import react from 'react'
 import RenameProject from './RenameProject'
+import { Pencil, XCircle } from 'react-bootstrap-icons'
 
-function Project(){
+function Project({project, edit}){
     return(
-<div className='Project'>
-        <RenameProject />
-</div>
+        <div className='Project' style={{
+            display: 'flex',
+            justifyContent: 'space-between',
+            alignItems: 'center',
+            padding: '0.25rem',
+            margin: '5px'
+        }}>
+            <div className="name">
+                {project.name}
+            </div>
+            <div className="btns" style={{
+                display: 'flex',
+                alignItems: 'center'
+            }}>
+                {edit ? (
+                    <div className="edit-delete" style={{
+                        display: 'flex',
+                        gap: '4px'
+                    }}>
+                        <span className="edit">
+                            <Pencil size="13" />
+                        </span>
+                        <span className="delete">
+                            <XCircle size="13" />
+                        </span>
+                    </div>
+                ) : (
+                    project.numOfTodos > 0 && (
+                        <div className="total-todos">
+                            {project.numOfTodos}
+                        </div>
+                    )
+                )}
+            </div>
+        </div>
     )
 }
 
--- todolist/src/Components/Projects.js
+++ todolist/src/Components/Projects.js
@@ -1,15 +1,55 @@
-import react from 'react'
+import react, { useState } from 'react'
 import AddNewProject from './AddNewProject'
 import Project from './Project'
-
+import { CaretUp, Palette, PencilFill } from 'react-bootstrap-icons'
 
 function Projects(){
-    
+    const [showMenu, setShowMenu] = useState(true);
+    const [edit, setEdit] = useState(true)
+    const pencilColor = edit ? "#1EC94C" : "#000000"
+
+    const projects = [
+        { id : 1, name : "personal", numOfTodos : 0},
+        { id : 2, name : "Work", numOfTodos : 2},
+        { id : 3, name : "School", numOfTodos : 3},
+    ]
+
     return(
-<div className='Projects'>
-<AddNewProject />
-<Project />
-</div>
+        <div className='Projects'>
+            <div className="header">
+                <div className="title">
+                    <Palette size="18" />
+                    <p>Projects</p>
+                </div>
+                <div className="btns" style={{
+                    display: 'flex',
+                    alignItems: 'center',
+                    gap: '8px'
+                }}>
+                    {
+                        showMenu && projects.length > 0 &&
+                        <span className='edit' onClick={() => setEdit (edit => !edit)}>
+                            <PencilFill size="15" color={pencilColor} />
+                        </span>
+                    }
+                    <AddNewProject />
+                    <span className='arrow'>
+                        <CaretUp size="20" />
+                    </span>
+                </div>
+            </div>
+            <div className="items">
+                {
+                    projects.map(project =>
+                        <Project
+                            project={project}
+                            key={project.id}
+                            edit={edit}
+                        />
+                    )
+                }
+            </div>
+        </div>
     )
 }
 
