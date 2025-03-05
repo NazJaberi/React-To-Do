7edf7f675b76b7e6a088a215d16a5e6dde019367
njaberi <nezarajaberi@gmail.com>
2025-03-05 04:43:46 +0300

    filters for projects and days

--- todolist/src/hooks/index.js
+++ todolist/src/hooks/index.js
@@ -1,8 +1,9 @@
 import { useState, useEffect } from 'react'
 import { db } from '../firebase'
 import { collection, onSnapshot } from 'firebase/firestore'
+import moment from 'moment'
 
-export function useTodos(){
+export function useTodos() {
     const [todos, setTodos] = useState([])
 
     useEffect(() => {
@@ -20,10 +21,40 @@ export function useTodos(){
     return todos
 }
 
-export function useProjects(todos){
+export function useFilterTodos(todos, selectedProject) {  // Fixed parameter name
+    const [filteredTodos, setFilteredTodos] = useState([])
+
+    useEffect(() => {
+        let data;
+        const todayDateFormated = moment().format('DD/MM/YYYY')
+
+        if (selectedProject === 'today') {
+            data = todos.filter(todo => todo.date === todayDateFormated)
+        } else if (selectedProject === 'next 7 days') {
+            data = todos.filter(todo => {
+                const todoDate = moment(todo.date, 'DD/MM/YYYY')
+                const todayDate = moment(todayDateFormated, 'DD/MM/YYYY')
+
+                const diffDays = todoDate.diff(todayDate, 'days')
+
+                return diffDays >= 0 && diffDays < 7
+            })
+        } else if (selectedProject === 'all days') {
+            data = todos
+        } else {
+            data = todos.filter(todo => todo.projectName === selectedProject)  // Fixed filter syntax
+        }
+
+        setFilteredTodos(data)
+    }, [todos, selectedProject])  // Added selectedProject to dependencies
+
+    return filteredTodos
+}
+
+export function useProjects(todos) {
     const [projects, setProjects] = useState([])
 
-    function calculateNumOfTodos(projectName, todos){
+    function calculateNumOfTodos(projectName, todos) {
         return todos.filter(todo => todo.projectName === projectName).length
     }
 
